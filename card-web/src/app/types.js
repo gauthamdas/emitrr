e5f3d328777ce12365e42d06c2c2f64b1a79526d
Gautham <gauthamd.das@gmail.com>
2023-05-12 17:52:28 +0530

    added leader board button

---
+++ card-web/src/app/types.js
@@ -0,0 +1,12 @@
+export const HOME_VIEW = 'HOME_VIEW';
+export const GAME_VIEW = 'GAME_VIEW';
+export const LEADERBOARD_VIEW = 'LEADERBOARD_VIEW';
+export const START_GAME = 'START_GAME';
+export const REMOVE_CARD = 'REMOVE_CARD';
+export const FLIPPED_CARD = 'FLIPPED_CARD';
+export const DEFUSE_CARD = 'DEFUSE_CARD';
+export const CAT_CARD = 'CAT_CARD';
+export const SHUFFLE_CARD = 'SHUFFLE_CARD';
+export const EXPLODING_KITTEN_CARD = 'EXPLODING_KITTEN_CARD';
+export const GAME_LOST = 'GAME_LOST';
+export const GAME_WON = 'GAME_WON';
