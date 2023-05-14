f57fb4c5210377d1835ce362270a7cea15719429
Gautham <gauthamd.das@gmail.com>
2023-05-14 12:09:53 +0530

    added game slice to store

--- card-web/src/app/store.js
+++ card-web/src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import userSlice from '../features/user/userSlice'
 import viewSlice from '../features/view/viewSlice'
 import leaderboardSlice from '../features/leaderboard/leaderboardSlice'
+import gameSlice from '../features/game/gameSlice'
 export default configureStore({
   reducer: {
     user: userSlice,
     view: viewSlice,
-    leaderboard: leaderboardSlice
+    leaderboard: leaderboardSlice,
+    game: gameSlice
   }
 })
